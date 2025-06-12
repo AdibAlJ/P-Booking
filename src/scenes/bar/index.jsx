f5a4d529219b17d6db11a9c6ae6860fbf3634c5c
AdibAlJ <adib23ti@mahasiswa.pcr.ac.id>
2025-06-12 21:55:42 +0700

    Test ulang

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -4,7 +4,7 @@ import { Header, BarChart } from "../../components";
 const Bar = () => {
   return (
     <Box m="20px">
-      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Header title="Bar Chart" subtitle="Simple bar test" />
       <Box height="75vh">
         <BarChart />
       </Box>
